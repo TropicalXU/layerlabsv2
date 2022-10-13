5349c90ef52a5e1aa15e89e29e8b950413ef4f56
Sean Hackett <seanhackett@Seans-MacBook-Pro.local>
2022-10-13 05:41:20 -0400

    final updates 8

--- src/components/ProjectComponent.js
+++ src/components/ProjectComponent.js
@@ -82,7 +82,7 @@ const Projects = (props) => {
                                 </p>
                             </div>
                             <div className='col-12 col-md-4'>
-                                <img src='/assets/images/develop.png' className='img-fluid' width='260px' height='260px' alt='Section'/>
+                                <img src='/assets/images/develop.png' className='img-fluid move-img' width='260px' height='260px' alt='Section'/>
                             </div>
                         </div>
                     </div>
